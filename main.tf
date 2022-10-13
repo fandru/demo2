a6a2c4b1eb88bb401d605670a6fe966d6fae27c9
fnelsonanburaj <39556131+fandru@users.noreply.github.com>
2022-10-13 13:30:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "backend_storage_git" {
-  name          = "mercaury_backend_data03"
+resource "google_storage_bucket" "backend_storage_dell" {
+  name          = "mercaury_backend_datadell"
   storage_class = "REGIONAL"
   project       = "demoproject-365313"
   location      = "us-central1"
